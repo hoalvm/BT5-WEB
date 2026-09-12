1992d8be0d1617016297d70be02b66be5ccc5fa4
hoalvm <lyhoa3011@gmail.com>
2026-09-12 11:54:52 +0700

    BT5-WEB

---
+++ src/main/java/hcmute/admin_crud/AdminCrudApplication.java
@@ -0,0 +1,13 @@
+package hcmute.admin_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminCrudApplication.class, args);
+	}
+
+}
